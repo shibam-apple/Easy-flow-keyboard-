5c6443a5dcb64d711096ba212701c899f77a7142
shibam-apple <bipuchattaraj906@gmail.com>
2026-08-24 13:22:53 +0530

    Migrate Kotlin JVM compiler options

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android {
@@ -15,8 +17,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
 }
+
+kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_17) } }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
